147f3f5004e7471f7423534884dc741e9eca99a3
Hrant <hrant.mailyan1999@gmail.com>
2026-01-01 19:09:57 +0200

    minor fix

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -17,7 +17,7 @@ func Authentication() gin.HandlerFunc {
 			return
 		}
 		claims, err := services.ValidateToken(ClientToken)
-		if err != nil {
+		if err != "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			c.Abort()
 			return
